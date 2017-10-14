a9851c1f14aea692a98a3eee424fdd066915d61c
Anthony COLAS <anthonycolas.france@gmail.com>
2017-10-14 16:10:14 +0900

    Modif printf.c

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -25,4 +25,73 @@ int		ft_printf(const char *fm, ...)
 		va_ends(args);
 		if (ret > INT_MAX)
 			return (ERROR);
+		else
+			return((int)ret);
+}
+
+int		ft_is_percent(va_list *args, const char **fm, t_print **list, size_t *ret)
+{
+	if (*(*fm) == '\0')
+		return (ERROR);
+	else if (*(fm) == '%')
+	{
+		write(1, "%", 1);
+		(*ret)++;
+	}
+	else
+	{
+		*list = ft_new_list(*list);
+		*fm = ft_get_format(*list, *fm);
+		*ret = go_push_it(*list, args, *ret);
+	}
+	return (END);
+}
+
+dize_t	ft_check_fm(va_list *args, const char *fm, size_t ret)
+{
+	t_print *list;
+
+	list = NULL;
+	while (*fm)
+	{
+		while (*fm && *fm != '%')
+		{
+			write(1, fm++, 1);
+			ret++;
+		}
+		if (*fm && *fm++ == '%')
+		{
+			if (ft_if_percent(args, &fm, &list, &ret))
+				return (ret);
+		}
+		if (*fm)
+			fm++;
+	}
+	return (ret);
+}
+
+size_t	go_push_it(t_print *list, va_list *args, size_t ret)
+{
+	ft_get_arg(list, args);
+	if(*list->buf) == '0'&& (UOCT(list->conversion))
+		&& list->dot && !(list->precision) && SHARP(list->flag))
+	{
+		ft_putchar('0');
+		ret++;
+	}
+	else if (STRING(list->conversion) && list->dot && !(list->precision)
+		&& list->width)
+		ft_print_num_wchar_zero(list, &ret);
+	else if (*list->buf) == '0'&& SYMB(list->conversion)
+		&& list->dot && list->width && ZERO(list->flag) && !MINUS(list->flag)
+		ft_print_null_char(list, &ret);
+	else if (*list->buf) == '0'&& NUM(list->conversion)
+		&& list->dot && !(list->precision) && list->width)
+		ft_print_num_wchar_zero(list, &ret);
+	else if (*list->buf) == '0'&& (NUM(list->conversion))
+		&& list->dot && !(list->precision) && !list->width)
+		;
+	else
+		ft_print_arg(list, &ret);
+	return (ret);
 }
