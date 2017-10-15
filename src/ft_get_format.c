4d4851a8d84c406e0d0273a5e46aa879f166a107
Anthony COLAS <anthonycolas.france@gmail.com>
2017-10-15 17:23:36 +0900

    size / flag

--- src/ft_get_format.c
+++ src/ft_get_format.c
@@ -38,3 +38,40 @@ const char      *ft_get_format(t_print *list, const char *fm)
     ft_check_converse(list, &fm);
     return (fm);
 }
+
+void            ft_check_converse(t_print *list, const char **fm)
+{
+    if (**fm && CONVERSE(**fm))
+        list->conversion = **fm;
+    else if (*fm && !(CONVERSE(**fm)))
+    {
+        list->buf = ft_memalloc(2);
+        *(list->buf) = **fm;
+    }
+}
+
+void            ft_get_flag(t_print *list, char fm)
+{
+    if (fm == '#')
+        list->flag |= 0x01;
+    else if (fm == '0')
+        list->flag |= 0x01 << 1;
+    else if (fm == '-')
+        list->flag |= 0x01 << 2;
+    else if (fm == '+')
+        list->flag |= 0x01 << 3;
+    else if (fm == ' ')
+        list->flag |= 0x01 << 4;
+}
+
+void            ft_get_size(t_print *list, char fm)
+{
+    if (fm == 'l')
+        list->l++;
+    else if (fm == 'h')
+        list->h++;
+    else if (fm == 'z')
+        list->z = 1;
+    else if (fm == 'j')
+        list->j = 1;
+}
